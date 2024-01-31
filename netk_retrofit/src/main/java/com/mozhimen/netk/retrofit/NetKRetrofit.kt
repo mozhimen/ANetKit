169eb0fb8df02fe65628b4aacf914d81fe471424
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-01-31 21:57:35 +0800

    0131

--- netk_retrofit/src/main/java/com/mozhimen/netk/retrofit/NetKRetrofit.kt
+++ netk_retrofit/src/main/java/com/mozhimen/netk/retrofit/NetKRetrofit.kt
@@ -7,7 +7,6 @@ import com.mozhimen.basick.manifestk.cons.CPermission
 import com.mozhimen.basick.utilk.android.util.UtilKLog
 import com.mozhimen.basick.utilk.bases.BaseUtilK
 import com.mozhimen.basick.utilk.squareup.moshi.UtilKMoshi
-import com.mozhimen.netk.retrofit.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
